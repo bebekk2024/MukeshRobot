cdf53d370df8b5d955132815e2bb0aec224c836d
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2025-11-19 01:52:30 +0700

    Update handlers.py

--- MukeshRobot/modules/helper_funcs/handlers.py
+++ MukeshRobot/modules/helper_funcs/handlers.py
@@ -5,7 +5,8 @@ from pyrate_limiter import (
     MemoryListBucket,
     RequestRate,
 )
-from telegram.ext import CommandHandler, MessageHandler, filters
+from telegram import Update
+from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler
 
 import MukeshRobot.modules.sql.blacklistusers_sql as sql
 from MukeshRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
@@ -13,22 +14,24 @@ from MukeshRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 if ALLOW_EXCL:
     CMD_STARTERS = ("/", "!")
 else:
-    CMD_STARTERS = ("/",)
+    CMD_STARTERS = "/"
+
 
 class AntiSpam:
     def __init__(self):
         self.whitelist = (
-            list(DEV_USERS or [])
-            + list(DRAGONS or [])
-            + list(WOLVES or [])
-            + list(DEMONS or [])
-            + list(TIGERS or [])
+            (DEV_USERS or [])
+            + (DRAGONS or [])
+            + (WOLVES or [])
+            + (DEMONS or [])
+            + (TIGERS or [])
         )
-        Duration.CUSTOM = 15
-        self.sec_limit = RequestRate(6, Duration.CUSTOM)
-        self.min_limit = RequestRate(20, Duration.MINUTE)
-        self.hour_limit = RequestRate(100, Duration.HOUR)
-        self.daily_limit = RequestRate(1000, Duration.DAY)
+        # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
+        Duration.CUSTOM = 15  # Custom duration, 15 seconds
+        self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
+        self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
+        self.hour_limit = RequestRate(100, Duration.HOUR)  # 100 / Per hour
+        self.daily_limit = RequestRate(1000, Duration.DAY)  # 1000 / Per day
         self.limiter = Limiter(
             self.sec_limit,
             self.min_limit,
@@ -38,6 +41,9 @@ class AntiSpam:
         )
 
     def check_user(self, user):
+        """
+        Return True if user is to be ignored else False
+        """
         if user in self.whitelist:
             return False
         try:
@@ -46,20 +52,87 @@ class AntiSpam:
         except BucketFullException:
             return True
 
+
 SpamChecker = AntiSpam()
 MessageHandlerChecker = AntiSpam()
 
-# ========== Handler Async untuk PTB v20+ ==========
-async def start(update, context):
-    await update.message.reply_text("Bot PTB v20+ siap digunakan!")
 
-async def handle_text_message(update, context):
-    await update.message.reply_text("Ini pesan teks biasa.")
+class CustomCommandHandler(CommandHandler):
+    def __init__(self, command, callback, admin_ok=False, allow_edit=False, **kwargs):
+        super().__init__(command, callback, **kwargs)
 
-async def handle_regex(update, context):
-    await update.message.reply_text("Terdeteksi pattern regex!")
+        if allow_edit is False:
+            self.filters &= ~(
+                Filters.update.edited_message | Filters.update.edited_channel_post
+            )
 
-# Handler objek siap didaftarkan di __init__.py
-CustomCommandHandler = CommandHandler("start", start)
-CustomMessageHandler = MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text_message)
-CustomRegexHandler = MessageHandler(filters.Regex(r"pattern"), handle_regex)
+    def check_update(self, update):
+        if isinstance(update, Update) and update.effective_message:
+            message = update.effective_message
+
+            try:
+                user_id = update.effective_user.id
+            except:
+                user_id = None
+
+            if user_id:
+                if sql.is_user_blacklisted(user_id):
+                    return False
+
+            if message.text and len(message.text) > 1:
+                fst_word = message.text.split(None, 1)[0]
+                if len(fst_word) > 1 and any(
+                    fst_word.startswith(start) for start in CMD_STARTERS
+                ):
+
+                    args = message.text.split()[1:]
+                    command = fst_word[1:].split("@")
+                    command.append(message.bot.username)
+                    if user_id == 1087968824:
+                        user_id = update.effective_chat.id
+                    if not (
+                        command[0].lower() in self.command
+                        and command[1].lower() == message.bot.username.lower()
+                    ):
+                        return None
+                    if SpamChecker.check_user(user_id):
+                        return None
+                    filter_result = self.filters(update)
+                    if filter_result:
+                        return args, filter_result
+                    else:
+                        return False
+
+    def handle_update(self, update, dispatcher, check_result, context=None):
+        if context:
+            self.collect_additional_context(context, update, dispatcher, check_result)
+            return self.callback(update, context)
+        else:
+            optional_args = self.collect_optional_args(dispatcher, update, check_result)
+            return self.callback(dispatcher.bot, update, **optional_args)
+
+    def collect_additional_context(self, context, update, dispatcher, check_result):
+        if isinstance(check_result, bool):
+            context.args = update.effective_message.text.split()[1:]
+        else:
+            context.args = check_result[0]
+            if isinstance(check_result[1], dict):
+                context.update(check_result[1])
+
+
+class CustomRegexHandler(RegexHandler):
+    def __init__(self, pattern, callback, friendly="", **kwargs):
+        super().__init__(pattern, callback, **kwargs)
+
+
+class CustomMessageHandler(MessageHandler):
+    def __init__(self, filters, callback, friendly="", allow_edit=False, **kwargs):
+        super().__init__(filters, callback, **kwargs)
+        if allow_edit is False:
+            self.filters &= ~(
+                Filters.update.edited_message | Filters.update.edited_channel_post
+            )
+
+        def check_update(self, update):
+            if isinstance(update, Update) and update.effective_message:
+                return self.filters(update)
